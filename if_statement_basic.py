c01a62d44ad46e568b732227b21fe32ec2a32769
Kylenn-Cortez <kylennrhysscortez_bsit@plmun.edu.ph>
2025-02-13 19:53:12 +0800

    Submitting Python Week 04 - Laboratory # 08

---
+++ if_statement_basic.py
@@ -0,0 +1,11 @@
+# Kylenn Rhyss Cortez
+# ITELEC2
+# Laboratory # 08 - Guided Coding Exercise: Simple if Statement in Python
+
+def main():
+    temperature = 35  # Or any other number
+    if temperature > 30:
+            print("It's a hot day!")  # Indented 4 spaces
+    
+if __name__ == "__main__":
+    main()
